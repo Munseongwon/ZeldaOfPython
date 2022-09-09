6bd941d9e7de92e825c5116c5015bf0a670dd6eb
seongwon <seongwon819@itc.mjc.ac.kr>
2022-09-10 00:18:57 +0900

    Mini Zelda of PyGame

---
+++ code/main.py
@@ -0,0 +1,45 @@
+import pygame, sys
+from settings import *
+from level import Level
+
+
+class Game:
+    def __init__(self):
+
+        # general setup
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption('Legend Of Zelda')
+        self.clock = pygame.time.Clock()
+
+        self.level = Level()
+
+        # sound
+        main_sound = pygame.mixer.Sound('../audio/main.ogg')
+        main_sound.set_volume(0.5)
+        main_sound.play(loops=-1)
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_m:
+                        self.level.toggle_menu()
+
+            self.screen.fill(WATER_COLOR)
+            self.level.run()
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+            self.screen.fill('black')
+            self.level.run()
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
